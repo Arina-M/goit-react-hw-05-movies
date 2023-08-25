6cb8624cc4392bf7f7c1b0b47a37146aa22289aa
Arina-M <79450238+Arina-M@users.noreply.github.com>
2023-08-25 14:45:04 +0000

    Deploying to gh-pages from @ Arina-M/goit-react-hw-05-movies@e8bdea1ad0c9f5c4478735cbe97f584d83e191e7 🚀

---
+++ static/js/545.dfa32a37.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[545],{1545:function(t,L,e){e.d(L,{FyZ:function(){return i}});var a=e(9983);function i(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 32 32"},child:[{tag:"path",attr:{d:"M 8 5 L 8 27 L 10 27 L 10 16.5 L 10.4375 16.8125 L 23.4375 25.8125 L 25 26.90625 L 25 5.09375 L 23.4375 6.1875 L 10.4375 15.1875 L 10 15.5 L 10 5 Z M 23 8.90625 L 23 23.09375 L 12.75 16 Z"}}]})(t)}}}]);
+//# sourceMappingURL=545.dfa32a37.chunk.js.map
